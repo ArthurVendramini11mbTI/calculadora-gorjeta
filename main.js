8d5bec251273aadf590da277d49b469eb1fcf438
vendraminiarthur2008 <vendraminiarthur2008@gmail.com>
2025-03-30 21:03:36 +0000

    finalmente é possivel calcular tudo que o projeto se propoe, contudo ainda faltam coisas

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
 //receber o valor da conta
 let billInput = document.querySelector("#bill")
 billInput.addEventListener("input", recieveBillValue)
 
 function recieveBillValue() {
-    bill = billInput.valuaAsNumber
+    bill = billInput.valueAsNumber
     calculate()
 }
 
@@ -16,18 +17,49 @@ let numberOfPeopleInput = document.querySelector("#people")
 numberOfPeopleInput.addEventListener("input", recieveNumberOfPeople)
 
 function recieveNumberOfPeople(){
-    numberOfPeople = numberOfPeopleInput.valuaAsNumber
+    numberOfPeople = numberOfPeopleInput.valueAsNumber
     calculate()
 }
 
 function recieveTipPercentage(value){
-    let buttonSelected = document.querySelector("button")
+    if(buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+    }
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add(`button-selected`)
+    tipPercentage = value / 100
+
+    let customTipInput = document.querySelector("#custom-tip")
+    customTipInput.value = ""
+
+    calculate()
+}
+
+function receiveCustomTipPercentage(){
+    let customTipInput = document.querySelector("#custom-tip")
+    tipPercentage = customTipInput.valueAsNumber / 100
+
+    if(buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
+    }
+
+    calculate()
 }
 
 
 function calculate(){
     if(bill != 0 && tipPercentage != 0 && numberOfPeople != 0){
-        console.log("Calcular")
+        let tipAmountStrong = document.querySelector(".amount strong")
+        let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+        tipAmountStrong.innerText = `$ ${tipAmountPerson.toFixed(2)}`
+
+        let totalStrong = document.querySelector(".total strong")
+
+        let total = (bill / numberOfPeople) + tipAmountPerson
+
+        totalStrong.innerText = `$ ${total.toFixed(2)}`
     }else{
         console.log("Ainda não é possível calcular")
     }
